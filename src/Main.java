829abd6e9a77f909950067744e1830b8b3ca1e5c
roberto <rvpassos@hotmail.com>
2023-08-01 20:21:40 -0300

    organizando em menus

--- src/Main.java
+++ src/Main.java
@@ -8,51 +8,75 @@ public class Main {
 
         List<Pessoa> pessoas = new ArrayList<>();
 
+        do{
+            System.out.println("[Menu Principal]\n1 - Cadastrar\n2 - Listar Pessoas\n3 - Encontrar Pessoa\n4 - Sair");
 
+            int opc = scanner.nextInt();
 
-        System.out.println("Quantas pessoas dejesa cadastrar?");
+            switch(opc){
+                case 1:
+                    System.out.println("Quantas pessoas dejesa cadastrar?");
 
-        int numPessoas = scanner.nextInt();
+                    int numPessoas = scanner.nextInt();
 
-        for(int i = 0; i < numPessoas; i++){
-            System.out.println("Digite o nome da pessoa: ");
-            String nome = scanner.next();
+                    for(int i = 0; i < numPessoas; i++){
+                        System.out.println("Digite o nome da pessoa: ");
+                        String nome = scanner.next();
 
-            System.out.println("Digite a idade da pessoa: ");
-            int idade = scanner.nextInt();
+                        System.out.println("Digite a idade da pessoa: ");
+                        int idade = scanner.nextInt();
 
-            System.out.println("Quantos Enderecos essa pessoa tem?");
-            int numEnderecos = scanner.nextInt();
+                        System.out.println("Quantos Enderecos essa pessoa tem?");
+                        int numEnderecos = scanner.nextInt();
 
-            List<Endereco> enderecos = new ArrayList<>();
+                        List<Endereco> enderecos = new ArrayList<>();
 
-            for(int j = 0; j < numEnderecos; j++ ){
-                System.out.println("Digite o nome da rua:");
-                String rua = scanner.next();
+                        for(int j = 0; j < numEnderecos; j++ ){
+                            System.out.println("Digite o nome da rua:");
+                            String rua = scanner.next();
 
-                System.out.println("Digite o numero da rua:");
-                int numero = scanner.nextInt();
+                            System.out.println("Digite o numero da rua:");
+                            int numero = scanner.nextInt();
 
-                enderecos.add(new Endereco(rua, numero));
+                            enderecos.add(new Endereco(rua, numero));
 
+                        }
+                        pessoas.add(new Pessoa(nome, idade, enderecos));
+
+                    }
+                    break;
+                case 2:
+                    pessoas.forEach(i -> {
+                        System.out.println("\nNome:" + i.getNome() + "\nIdade: " + i.getIdade());
+
+                        for (Endereco j : i.getEnderecos()) {
+                            System.out.println("Rua: " + j.getRua() + "\nNumero: " + j.getNumero());
+                        }
+                        System.out.println("\n--- --- --- ---");
+                    });
+                    break;
+                case 3:
+                    System.out.println("\nDigite o nome da pessoa");
+
+                    String nomedapessoa = scanner.next();
+
+                    pessoas.forEach(i -> {
+                        if (i.getNome().equals(nomedapessoa)) {
+                            System.out.println("\nPessoa Encontrada!\n");
+                            System.out.println("Nome: " + i.getNome() + "\nIdade: " + i.getIdade());
+
+                            for (Endereco j : i.getEnderecos()) {
+                                System.out.println("Rua: " + j.getRua() + "\nNumero: " + j.getNumero() + "\n");
+                            }
+                        }
+                    });
+                    break;
+                case 4:
+                    System.exit(1);
+                    break;
+                default:
+                    System.out.println("Opcao invalida!");
             }
-            pessoas.add(new Pessoa(nome, idade, enderecos));
-
-        }
-
-        System.out.println("\nDigite o nome da pessoa");
-
-        String nomedapessoa = scanner.next();
-
-        pessoas.forEach(i -> {
-            if (i.getNome().equals(nomedapessoa)) {
-                System.out.println("\nNome: " + i.getNome() + "\nIdade: " + i.getIdade());
-
-                for (Endereco j : i.getEnderecos()) {
-                    System.out.println("Rua: " + j.getRua() + "\nNumero: " + j.getNumero());
-                }
-            }
-        });
-
+        }while(1==1);
     }
 }
